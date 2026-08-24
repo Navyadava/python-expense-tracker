9226ffb35a1bc79a932dfb7e407e050eeefa3d64
Navya Dava <navyadava0912@gmail.com>
2026-08-24 03:34:18 -0400

    Create initial expense tracker menu

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Expense Tracker")
+print("----------------")
+print("1. Add expense")
+print("2. View expenses")
+print("3. Show total")
+print("4. Exit")
+
+choice = input("Enter your choice (1-4): ")
+
+print(f"You selected option {choice}")
