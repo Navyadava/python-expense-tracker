2d590b3493746bc6634d40cec9ac3191e9fcb1c8
Navya Dava <navyadava0912@gmail.com>
2026-08-25 07:01:55 -0400

    Add expense functions and in-memory storage

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
+expenses = []
+
+def add_expense():
+    amount = float(input("enter expense amount: $"))
+    category = input("enter category")
+    date = input("enter date (YYYY-MM-DD): ")
+    note = input("enter note: ")
+
+expense = {
+    "amount": 250.0,
+    "category": "Food",
+    "date": "2026-08-15",
+    "note": "Lunch" 
+}
+
+expenses.append(expense)
+print("expense added successfully!")
+
+def view_expenses():
+    if len(expense) == 0:
+        print("no expense found.")
+        return
+
+    print("\nsaves expenses")
+    print("----------------")
+
+    for expense in expenses:
+        print(f"Amount: ${expense['amount']:.2f}")
+        print(f"Category: {expense['category']}")
+        print(f"Date: {expense['date']}")
+        print(f"Note: {expenses['note']}")
+        print("--------------")
+
+def calculate_total():
+    total = 0
+
+    for expense in expenses:
+        total = total + expense["amount"]
+
+    print(f"Total expenses: ${total:.2f}")
+
 while True:
     print("\nExpense Tracker")
     print("----------------")
@@ -9,13 +50,13 @@ while True:
     choice = input("Enter your choice (1-4): ")
 
     if choice == "1":
-        print("Add expense selected")
+        add_expense()
 
     elif choice == "2":
-        print("View expenses selected")
+        view_expenses()
 
     elif choice == "3":
-        print("Show total selected")
+        calculate_total()
 
     elif choice == "4":
         print("Goodbye!")
