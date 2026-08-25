9a3e3704f1c5136f8b31932b2de1905a481b3502
Navya Dava <navyadava0912@gmail.com>
2026-08-25 04:01:33 -0400

    Add menu loop and input validation

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-print("Expense Tracker")
-print("----------------")
-print("1. Add expense")
-print("2. View expenses")
-print("3. Show total")
-print("4. Exit")
+while True:
+    print("\nExpense Tracker")
+    print("----------------")
+    print("1. Add expense")
+    print("2. View expenses")
+    print("3. Show total")
+    print("4. Exit")
 
-choice = input("Enter your choice (1-4): ")
+    choice = input("Enter your choice (1-4): ")
 
-print(f"You selected option {choice}")
+    if choice == "1":
+        print("Add expense selected")
+
+    elif choice == "2":
+        print("View expenses selected")
+
+    elif choice == "3":
+        print("Show total selected")
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Please enter a valid option.")
